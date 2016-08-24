8b5dc3df345045554ac32aed46803d5c4daeae19
Rubén Díaz Martínez <rubendm92@gmail.com>
2016-08-24 17:13:35 +0200

    Solve kata in Java

---
+++ src/main/java/me/rubendm/scrabblesets/ScrabbleSets.java
@@ -0,0 +1,89 @@
+package me.rubendm.scrabblesets;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.*;
+
+class ScrabbleSets {
+
+    private final List<TileCount> initialTileCount;
+
+    ScrabbleSets(Map<Character, Long> set) {
+        initialTileCount = set.entrySet().stream()
+                .map(TileCount::new)
+                .collect(toList());
+    }
+
+    String tilesLeft(String input) {
+        final Map<Long, List<Character>> outputMap = countLeftCharacters(inputCharactersByCount(input));
+        if (hasBeenSubtractedMoreCharactersThanAllowed(outputMap)) {
+            return invalidInputMessage(outputMap);
+        }
+        return formatOutput(outputMap);
+    }
+
+    private Map<Character, Long> inputCharactersByCount(String input) {
+        return characters(input)
+                .collect(groupingBy(identity(), counting()));
+    }
+
+    private Stream<Character> characters(String input) {
+        return input.chars().mapToObj(c -> (char) c);
+    }
+
+    private Map<Long, List<Character>> countLeftCharacters(Map<Character, Long> inputMap) {
+        return initialTileCount.stream()
+                .map(tile -> tile.minus(inputMap.getOrDefault(tile.character, 0L)))
+                .collect(groupingBy(tile -> tile.count, mapping(tile -> tile.character, toList())));
+    }
+
+    private boolean hasBeenSubtractedMoreCharactersThanAllowed(Map<Long, List<Character>> outputMap) {
+        return outputMap.entrySet().stream().anyMatch(entry -> entry.getKey() < 0);
+    }
+
+    private String invalidInputMessage(Map<Long, List<Character>> outputMap) {
+        return outputMap.entrySet().stream()
+                .filter(entry -> entry.getKey() < 0)
+                .map(Map.Entry::getValue)
+                .flatMap(Collection::stream)
+                .map(c -> c + "'s")
+                .collect(joining(", ", "Invalid input. More ", " have been taken from the bag than possible."));
+    }
+
+    private String formatOutput(Map<Long, List<Character>> outputMap) {
+        return outputMap.entrySet().stream()
+                .sorted((left, right) -> right.getKey().compareTo(left.getKey()))
+                .map(this::formatLine)
+                .collect(joining("\n"));
+    }
+
+    private String formatLine(Map.Entry<Long, List<Character>> entry) {
+        return entry.getKey() + ": " + entry.getValue().stream()
+                .sorted()
+                .map(Object::toString)
+                .collect(joining(", "));
+    }
+
+    private static class TileCount {
+        final Character character;
+        final Long count;
+
+        TileCount(Character character, Long count) {
+            this.character = character;
+            this.count = count;
+        }
+
+        TileCount(Map.Entry<Character, Long> entry) {
+            this.character = entry.getKey();
+            this.count = entry.getValue();
+        }
+
+        TileCount minus(Long minus) {
+            return new TileCount(character, count - minus);
+        }
+    }
+}
---
+++ src/test/java/me/rubendm/scrabblesets/ScrabbleSetsShould.java
@@ -0,0 +1,93 @@
+package me.rubendm.scrabblesets;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+@RunWith(Parameterized.class)
+public class ScrabbleSetsShould {
+
+    private ScrabbleSets scrabble;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return asList(new Object[][] {
+                { "PQAREIOURSTHGWIOAE_", "10: E\n" +
+                        "7: A, I\n" +
+                        "6: N, O\n" +
+                        "5: T\n" +
+                        "4: D, L, R\n" +
+                        "3: S, U\n" +
+                        "2: B, C, F, G, M, V, Y\n" +
+                        "1: H, J, K, P, W, X, Z, _\n" +
+                        "0: Q" },
+                {"LQTOONOEFFJZT", "11: E\n" +
+                        "9: A, I\n" +
+                        "6: R\n" +
+                        "5: N, O\n" +
+                        "4: D, S, T, U\n" +
+                        "3: G, L\n" +
+                        "2: B, C, H, M, P, V, W, Y, _\n" +
+                        "1: K, X\n" +
+                        "0: F, J, Q, Z"},
+                {"AXHDRUIOR_XHJZUQEE", "Invalid input. More X's have been taken from the bag than possible."},
+        });
+    }
+
+    @Parameterized.Parameter
+    public String input;
+    @Parameterized.Parameter(value = 1)
+    public String expectedOutput;
+
+    private static final Map<Character, Long> englishScrabbleSet = new HashMap<>();
+    static {
+        englishScrabbleSet.put('A', 9L);
+        englishScrabbleSet.put('B', 2L);
+        englishScrabbleSet.put('C', 2L);
+        englishScrabbleSet.put('D', 4L);
+        englishScrabbleSet.put('E', 12L);
+        englishScrabbleSet.put('F', 2L);
+        englishScrabbleSet.put('G', 3L);
+        englishScrabbleSet.put('H', 2L);
+        englishScrabbleSet.put('I', 9L);
+        englishScrabbleSet.put('J', 1L);
+        englishScrabbleSet.put('K', 1L);
+        englishScrabbleSet.put('L', 4L);
+        englishScrabbleSet.put('M', 2L);
+        englishScrabbleSet.put('N', 6L);
+        englishScrabbleSet.put('O', 8L);
+        englishScrabbleSet.put('P', 2L);
+        englishScrabbleSet.put('Q', 1L);
+        englishScrabbleSet.put('R', 6L);
+        englishScrabbleSet.put('S', 4L);
+        englishScrabbleSet.put('T', 6L);
+        englishScrabbleSet.put('U', 4L);
+        englishScrabbleSet.put('V', 2L);
+        englishScrabbleSet.put('W', 2L);
+        englishScrabbleSet.put('X', 1L);
+        englishScrabbleSet.put('Y', 2L);
+        englishScrabbleSet.put('Z', 1L);
+        englishScrabbleSet.put('_', 2L);
+    }
+
+    @Before
+    public void setUp() {
+        scrabble = new ScrabbleSets(englishScrabbleSet);
+    }
+
+    @Test
+    public void output_the_tiles_that_are_left_in_the_bag() {
+        String output = scrabble.tilesLeft(input);
+
+        assertThat(output, is(expectedOutput));
+    }
+}
